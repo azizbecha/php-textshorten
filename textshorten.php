71b14908ef254db2b4b6e430afc43adcc563bf9d
Aziz Becha <63454940+AzizVirus@users.noreply.github.com>
2021-07-27 20:50:02 +0100

    Update textshorten.php

--- textshorten.php
+++ textshorten.php
@@ -1,1 +1,11 @@
+<?php 
+function textShorten($text, $limit){
+  if (strlen($text) > $limit) {
+    $maxLength = 14;
+    return substr($text, 0, $limit). " ...";
+} else {
+  return $text;
+}
 
+}
+?>
