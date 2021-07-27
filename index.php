ed823af8ce95ac5cb9ede71ec82ed5f0be3c6f9b
Aziz Becha <63454940+AzizVirus@users.noreply.github.com>
2021-07-27 23:12:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php 
+
+// Import the function
 include("textshorten.php");
 
+// String to shorten
 $string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+// String shortned
 echo textShorten($string, 5); //output will be ABCDE
 
 ?>
