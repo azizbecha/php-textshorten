a37bf73ee2f07c2b14f0bf571c416b45902da964
Aziz Becha <63454940+AzizVirus@users.noreply.github.com>
2021-07-27 23:12:53 +0100

    Update textshorten.php

--- textshorten.php
+++ textshorten.php
@@ -1,5 +1,7 @@
 <?php 
 function textShorten($text, $limit){
+  
+  // If the string length is greater than limit: 
   if (strlen($text) > $limit) {
     $maxLength = 14;
     return substr($text, 0, $limit). " ...";
